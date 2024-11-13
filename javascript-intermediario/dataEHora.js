ba350487fdce47238fa2ffd79cebfccadf5f1ce4
pablo <devpabloh@gmail.com>
2024-11-13 17:27:09 -0300

    puxando horas, minutos, segundos, dia, mês, ano

--- javascript-intermediario/dataEHora.js
+++ javascript-intermediario/dataEHora.js
@@ -18,3 +18,26 @@ console.log(new Date(2024,6,3,14,30,0))
 console.log(new Date("2024-07-03T17:05:00"))
 
 console.log(new Date("july 3, 2032 17:06:00"))
+
+let date = new Date("2024-07-02T14:30:10")
+
+// Dia da semana de 0 à 6 (domingo é 0)
+console.log(date.getDay())
+
+// Dia do mês (0 à 30)
+console.log(date.getDate())
+
+// Mês (0 à 11)
+console.log(date.getMonth() + 1)
+
+// Ano
+console.log(date.getFullYear())
+
+// Horas
+console.log(date.getHours())
+
+// Minutos
+console.log(date.getMinutes())
+
+// Segundos
+console.log(date.getSeconds())
