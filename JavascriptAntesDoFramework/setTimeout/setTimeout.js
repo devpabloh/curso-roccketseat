c9ef80b3a9c367b41ac452c4c424b08cb2526aa4
Pablo Henrique <pablohenriqueadm94@gmail.com>
2024-12-28 19:01:28 -0300

    aprendendo a utilizar o setTimeout()

---
+++ JavascriptAntesDoFramework/setTimeout/setTimeout.js
@@ -0,0 +1,5 @@
+// setTimeout() executa uma função após um intervalo de tempo especificado.
+
+setTimeout(()=>{
+    console.log("Olá meu querido!")
+}, 3000) // o tempo colocado aqui é em milissegundos
