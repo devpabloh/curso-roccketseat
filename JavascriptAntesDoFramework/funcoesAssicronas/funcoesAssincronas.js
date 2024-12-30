a5d3c8fe4c5c8c8197f2db7d140a7dc5aab62509
Pablo Henrique <pablohenriqueadm94@gmail.com>
2024-12-30 09:15:23 -0300

    conhecendo o async e o await

--- JavascriptAntesDoFramework/funcoesAssicronas/funcoesAssincronas.js
+++ JavascriptAntesDoFramework/funcoesAssicronas/funcoesAssincronas.js
@@ -15,13 +15,38 @@ function asyncFunction(){
     },3000) // Simula uma operação assicrona que eleva 3 segundos
     })
 }
+// USANDO O THEN CATCH E FINALLY
+/* console.log("Executando função assicrona")
 
-console.log("Executando função assicrona")
+asyncFunction()
+    .then((response)=>{
+        console.log(`Sucesso: ${response}`)
+    })
+    .catch((error)=>{
+        console.log(`Erro: ${error}`)
+    })
+    .finally(()=>{
+        console.log("Fim da execução!")
+    }) */
 
-asyncFunction().then((response)=>{
-    console.log(`Sucesso: ${response}`)
-}).catch((error)=>{
-    console.log(`Erro: ${error}`)
-}).finally(()=>{
-    console.log("Fim da execução!")
-})
+// colocamos o async para dizer que vamos utilizar uma função assincrona, que nos permite o uso do await para esperar a sua finalização
+
+/* async function fetch(){
+    const response = await asyncFunction() // colocamos o await para o javascript esperar a execução da função
+    console.log(response)
+}   */ 
+
+// Também podemos utilizar a função assincrona na notação de arow function
+const fetch = async () => {
+    try{
+        const response = await asyncFunction()
+        console.log("Sucesso", response)
+    }catch(error){
+        console.log("error: ", error)
+    }finally{
+        console.log("Fim da execução!")
+    }
+    
+}
+
+fetch()
