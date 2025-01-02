c728d2b90aeb3cc1a1f68ee305253480c28bcaa2
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-01-01 23:29:32 -0300

    Aprendendo a utilizar pacotes / bibliotecas na nossa aplicação

---
+++ JavascriptAntesDoFramework/conhecendoEUtilizandoBibliotecas/main.js
@@ -0,0 +1,5 @@
+const now = dayjs()
+console.log(now.format('DD')) // Dia
+console.log(now.format("DD/MM")) // Dia e mês
+console.log(now.format("DD/MM/YYYY")) // Data completa
+console.log(now.format("DD/MM - HH:mm")) // Dia, mês, horas e minutos
