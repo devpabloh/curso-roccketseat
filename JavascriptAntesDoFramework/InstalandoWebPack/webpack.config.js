8a695fad28d961813ace31b8e2552473a857f030
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-01-04 23:07:13 -0300

    escolhendo as dependências de desenvolvimento css

--- JavascriptAntesDoFramework/InstalandoWebPack/webpack.config.js
+++ JavascriptAntesDoFramework/InstalandoWebPack/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/i,
-                use: []
+                use: ["style-loader", "css-loader"]
             }
             
         ]
