74228fba1ee5ba6a694fdc32f51b7eb173978fc6
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-01-15 22:28:12 -0300

    tentando girar a string

---
+++ teste js/teste.js
@@ -0,0 +1,11 @@
+function trasParaFrente(){
+    let palavra = "Javascript"
+    palavra.split().reverse().join()
+    return palavra
+}
+
+console.log(trasParaFrente())
+
+palavra = "javascript"
+palavra.split("").reverse().join("")
+console.log(palavra)
